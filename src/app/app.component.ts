3d95e14dd05469c6ea64f4632bf8cc0187c94a68
Roen Ylagan <roenylagan@live.com>
2018-05-16 20:52:54 +0800

    working binding and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/employee-service/v1/employees/younger/than/25', {responseType: 'json'});
+  }
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   itemCount: number = 4;
+  btnText: string = 'Add an Item';    // Add this line
+  goalText: string = 'My first life goal';
+  goals = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
   }
 
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+  
 }
